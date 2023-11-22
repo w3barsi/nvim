c8662491c5cfc6018f7eed6efb37582dc5643879
w3barsi <darcybalaga@gmail.com>
2023-11-22 22:57:07 +0800

    fixed folds overlapping with gitsigns

--- lua/custom/plugins/ufo.lua
+++ lua/custom/plugins/ufo.lua
@@ -53,9 +53,9 @@ return {
       require('statuscol').setup {
         relculright = true,
         segments = {
-          { text = { builtin.foldfunc },      click = 'v:lua.ScFa' },
           { text = { '%s' },                  click = 'v:lua.ScSa' },
-          { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+          { text = { builtin.lnumfunc, ' ' },      click = 'v:lua.ScLa' },
+          { text = { builtin.foldfunc, '  ' }, click = 'v:lua.ScFa' },
         },
       }
     end,
