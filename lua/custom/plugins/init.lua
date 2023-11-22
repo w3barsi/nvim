1d477dce263b9b782d8da482f6cd5c89c639a384
w3barsi <darcybalaga@gmail.com>
2023-11-22 23:07:27 +0800

    added persistence.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -133,5 +133,16 @@ return { -- NOTE: First, some plugins that don't require any configuration
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre',
+    opts = { options = vim.opt.sessionoptions:get() },
+    -- stylua: ignore
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+    },
+  },
 }
 
