a97244e02f1dc61b275ab2df2bed388114435f7e
w3barsi <darcybalaga@gmail.com>
2024-09-10 00:19:27 +0800

    fix(autotag): moved autotag to its own object to fix deprecated treesitter error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,53 +1,55 @@
--- return {}
 return {
-    -- Highlight, edit, and navigate code
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        -- 'nvim-treesitter/nvim-treesitter-context',
-        "nvim-treesitter/playground",
-
-        -- Auto closing tags for HTML
+    {
         "windwp/nvim-ts-autotag",
+        opts = {
+            opts = {
+                enable_close = true,
+                enable_close_on_slash = false
+            }
+        }
     },
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
+    {
+        -- Highlight, edit, and navigate code
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            -- 'nvim-treesitter/nvim-treesitter-context',
+            "nvim-treesitter/playground",
 
-        autotag = {
-            enable = true,
-            enable_close_on_slash = false,
+            -- Auto closing tags for HTML
         },
+        build = ":TSUpdate",
+        main = "nvim-treesitter.configs",
+        opts = {
+            -- Add languages to be installed here that you want installed for treesitter
+            ensure_installed = {
+                "c",
+                "cpp",
+                "go",
+                "lua",
+                "python",
+                "rust",
+                "tsx",
+                "javascript",
+                "typescript",
+                "vimdoc",
+                "vim",
+            },
 
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = {
-            "c",
-            "cpp",
-            "go",
-            "lua",
-            "python",
-            "rust",
-            "tsx",
-            "javascript",
-            "typescript",
-            "vimdoc",
-            "vim",
-        },
+            -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+            auto_install = true,
 
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-        auto_install = true,
-
-        highlight = { enable = true },
-        indent = { enable = false },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<enter>",
-                node_incremental = "<enter>",
-                scope_incremental = "<c-s>",
-                node_decremental = "<bs>",
+            highlight = { enable = true },
+            indent = { enable = false },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<enter>",
+                    node_incremental = "<enter>",
+                    scope_incremental = "<c-s>",
+                    node_decremental = "<bs>",
+                },
             },
         },
-    },
-}
+    } }
