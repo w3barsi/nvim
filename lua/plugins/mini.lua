d9c0c4c1a1ab56477bd9bfb715c424400318a15f
w3barsi <darcybalaga@gmail.com>
2024-10-09 18:33:29 +0800

    refactor(statusline): trying out lualine again

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,37 +1,43 @@
-return { -- Collection of various small independent plugins/modules
+return {
     'echasnovski/mini.nvim',
     config = function()
-        -- Better Around/Inside textobjects
+        -- local statusline = require 'mini.statusline'
         --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-        --  - ci'  - [C]hange [I]nside [']quote
-        -- require('mini.ai').setup { n_lines = 500 }
+        -- local my_active_content = function()
+        --     local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+        --     local git           = MiniStatusline.section_git({ trunc_width = 40 })
+        --     local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+        --     local diagnostics   = MiniStatusline.section_diagnostics({
+        --         signs = require("config").icons.diagnostics,
+        --         trunc_width = 75
+        --     })
+        --     local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+        --     local filename      = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":~")
+        --     return MiniStatusline.combine_groups({
+        --         { hl = mode_hl,                 strings = { mode } },
+        --         { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics, diff } },
+        --         '%<', -- Mark general truncate point
+        --         { hl = 'MiniStatuslineFilename', strings = { filename } },
+        --         '%=', -- End left alignment
+        --         {
+        --             hl = 'MiniStatuslineFileinfo',
+        --             strings = {
+        --                 vim.bo.filetype ~= ""
+        --                 and require("mini.icons").get("filetype", vim.bo.filetype) .. " " .. vim.bo.filetype,
+        --             },
+        --         },
+        --         { hl = mode_hl,                  strings = { "%l:%v" } },
+        --     })
+        -- end
 
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        -- require('mini.surround').setup()
-
-        -- Simple and easy statusline.
-        --  You could remove this setup call if you don't like it,
-        --  and try some other statusline plugin
-        local statusline = require 'mini.statusline'
-        -- set use_icons to true if you have a Nerd Font
-        statusline.setup { use_icons = vim.g.have_nerd_font }
-
-        -- You can configure sections in the statusline by overriding their
-        -- default behavior. For example, here we set the section for
-        -- cursor location to LINE:COLUMN
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function()
-            return '%2l:%-2v'
-        end
-
-        -- ... and there is more!
-        --  Check out: https://github.com/echasnovski/mini.nvim
+        -- statusline.setup({ use_icons = vim.g.have_nerd_font, content = { active = my_active_content } })
+        require('mini.files').setup({
+            options = {
+                use_as_default_explorer = false
+            }
+        })
     end,
+    keys = {
+        { "_", function() MiniFiles.open() end, desc = "Open mini_files" }
+    }
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,33 +1,40 @@
 return {
     "stevearc/oil.nvim",
-    config = function()
-        require("oil").setup({
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-s>"] = "actions.select_vsplit",
-                ["<C-h>"] = "actions.select_split",
-                ["<C-t>"] = "actions.select_tab",
-                ["gp"] = "actions.preview",
-                ["<C-c>"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["-"] = "actions.parent",
-                ["<BS>"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["`"] = "actions.cd",
-                ["~"] = "actions.tcd",
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-                ["g\\"] = "actions.toggle_trash",
-            },
-            use_default_keymaps = false,
-            view_options = {
-                show_hidden = true
-            }
-        })
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
+    lazy = false,
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        default_file_explorer = true,
+        keymaps = {
+            ["g?"] = "actions.show_help",
+            ["<CR>"] = "actions.select",
+            ["<C-s>"] = "actions.select_vsplit",
+            ["<C-h>"] = "actions.select_split",
+            ["<C-t>"] = "actions.select_tab",
+            ["gp"] = "actions.preview",
+            ["<C-c>"] = "actions.close",
+            ["<Esc>"] = "actions.close",
+            ["<C-l>"] = "actions.refresh",
+            ["-"] = "actions.parent",
+            ["<BS>"] = "actions.parent",
+            ["_"] = "actions.open_cwd",
+            ["`"] = "actions.cd",
+            ["~"] = "actions.tcd",
+            ["gs"] = "actions.change_sort",
+            ["gx"] = "actions.open_external",
+            ["g."] = "actions.toggle_hidden",
+            ["g\\"] = "actions.toggle_trash",
+        },
+        use_default_keymaps = false,
+        view_options = {
+            show_hidden = true
+        },
+    },
+    keys = {
+        { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+        -- { "_", "<CMD>Oil --float<CR>", desc = "Open parent directory in floating window" },
+    },
     -- Optional dependencies
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }
