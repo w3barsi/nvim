531ae3e6eb73659b501731af9c502465118e6d0b
w3barsi <darcybalaga@gmail.com>
2025-01-15 01:40:33 +0800

    feat(colorscheme): added integrations to plugins

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -36,9 +36,36 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        -- config = function()
-        --     vim.cmd.colorscheme("catppuccin")
-        -- end,
+        config = function()
+            require("catppuccin").setup({
+                integrations = {
+                    cmp = true,
+                    fidget = true,
+                    gitsigns = true,
+                    harpoon = true,
+                    indent_blankline = {
+                        enabled = false,
+                        scope_color = "sapphire",
+                        colored_indent_levels = false,
+                    },
+                    mason = true,
+                    native_lsp = { enabled = true },
+                    noice = true,
+                    notify = true,
+                    symbols_outline = true,
+                    telescope = true,
+                    treesitter = true,
+                    treesitter_context = true,
+                },
+                custom_highlights = function(colors)
+                    return {
+                        YankHighlight = { fg = colors.base, bg = colors.red },
+                    }
+                end
+            })
+
+            vim.cmd.colorscheme("catppuccin-macchiato")
+        end,
     },
 
     {
