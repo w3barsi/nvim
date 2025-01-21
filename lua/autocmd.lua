d433c1eb057c74164d26fe439e2264a41333f65f
w3barsi <darcybalaga@gmail.com>
2025-01-21 20:49:58 +0800

    feat(autocmd): added autocommands for resize and restoring cursor positions

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     group = highlight_group,
     pattern = "*",
 })
+
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
+})
