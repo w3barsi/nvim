3f5ac0be1ee786927171a34e2c5b7070e2b3543c
w3barsi <darcybalaga@gmail.com>
2025-05-30 15:03:20 +0800

    added convex/_generated to telescope ignore list

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,8 +18,8 @@ return {
         require("telescope").setup({
             pickers = {
                 find_files = {
-                    hidden = true
-                }
+                    hidden = true,
+                },
             },
             defaults = {
                 mappings = {
@@ -33,7 +33,8 @@ return {
                 },
                 file_ignore_patterns = {
                     "node_modules/*",
-                    ".git/*"
+                    ".git/*",
+                    "convex/_generated",
                 },
             },
         })
@@ -41,14 +42,10 @@ return {
         pcall(require("telescope").load_extension, "fzf")
 
         local builtin = require("telescope.builtin")
-        -- vim.keymap.set("n", "<ieader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-        -- vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-        vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "[S]earch by [G]rep" })
     end,
 }
