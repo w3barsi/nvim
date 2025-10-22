b632f90f162a578f1730e649f5270e8ea449a17f
w3barsi <darcybalaga@gmail.com>
2025-10-22 20:03:22 +0800

    changed color highlighting to different plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,16 +54,23 @@ return {
         cmd = "Screenkey",
         config = true,
     },
-    -- {
-    --     "norcalli/nvim-colorizer.lua",
-    --     opts = function()
-    --         vim.o.termguicolors = true
-    --         return {
-    --             "lua",
-    --             "typescriptreact",
-    --         }
-    --     end,
-    -- },
+    {
+        "eero-lehtinen/oklch-color-picker.nvim",
+        event = "VeryLazy",
+        version = "*",
+        keys = {
+            -- One handed keymap recommended, you will be using the mouse
+            {
+                "<leader>v",
+                function()
+                    require("oklch-color-picker").pick_under_cursor()
+                end,
+                desc = "Color pick under cursor",
+            },
+        },
+        ---@type oklch.Opts
+        opts = {},
+    },
     {
         "tzachar/highlight-undo.nvim",
         opts = {},
