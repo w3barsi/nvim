e7bb823883377f56742229ab98352e2b68cc0a4a
w3barsi <darcybalaga@gmail.com>
2025-10-22 20:00:50 +0800

    started using fff.nvim for c+p

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,51 +1,74 @@
 return {
+    {
 
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    event = "VeryLazy",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            cond = function()
-                return vim.fn.executable("make") == 1
-            end,
-        },
-    },
-    config = function()
-        local actions = require("telescope.actions")
-        require("telescope").setup({
-            pickers = {
-                find_files = {
-                    hidden = true,
-                },
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = function()
+                    return vim.fn.executable("make") == 1
+                end,
             },
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-u>"] = false,
-                        ["<C-d>"] = false,
-                        ["<C-j>"] = "move_selection_next",
-                        ["<C-k>"] = "move_selection_previous",
-                        ["<esc>"] = actions.close,
+        },
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                pickers = {
+                    find_files = {
+                        hidden = true,
                     },
                 },
-                file_ignore_patterns = {
-                    "node_modules/*",
-                    ".git/*",
-                    "convex/_generated",
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-u>"] = false,
+                            ["<C-d>"] = false,
+                            ["<C-j>"] = "move_selection_next",
+                            ["<C-k>"] = "move_selection_previous",
+                            ["<esc>"] = actions.close,
+                        },
+                    },
+                    file_ignore_patterns = {
+                        "node_modules/*",
+                        ".git/*",
+                        "convex/_generated",
+                    },
                 },
+            })
+
+            pcall(require("telescope").load_extension, "fzf")
+
+            local builtin = require("telescope.builtin")
+            -- vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
+            vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+        end,
+    },
+    {
+        "dmtrKovalenko/fff.nvim",
+        build = "cargo build --release",
+        -- or if you are using nixos
+        -- build = "nix run .#release",
+        opts = {
+            keymaps = {
+                move_up = { "<Up>", "<C-k>", "<Tab>" }, -- Multiple bindings supported
+                move_down = { "<Down>", "<C-j>", "<S-Tab>" },
             },
-        })
-
-        pcall(require("telescope").load_extension, "fzf")
-
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-    end,
+        },
+        keys = {
+            {
+                "<C-p>", -- try it if you didn't it is a banger keybinding for a picker
+                function()
+                    require("fff").find_files() -- or find_in_git_root() if you only want git files
+                end,
+                desc = "Open file picker",
+            },
+        },
+    },
 }
