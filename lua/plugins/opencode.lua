2c6581fa5e417b0a58fc7813da2356c91e2c0614
w3barsi <darcybalaga@gmail.com>
2026-02-19 02:20:52 +0800

    fix trying to get opencode.nvim to work again

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,47 +1,44 @@
 return {
-
-    "NickvanDyke/opencode.nvim",
+    "nickjvandyke/opencode.nvim",
+    version = "*", -- Latest stable release
     dependencies = {
-        -- Recommended for better prompt input, and required to use `opencode.nvim`'s embedded terminal — otherwise optional
-        { "folke/snacks.nvim", opts = { input = { enabled = true } } },
+        {
+            -- `snacks.nvim` integration is recommended, but optional
+            ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+            "folke/snacks.nvim",
+            optional = true,
+            opts = {
+                input = {}, -- Enhances `ask()`
+                picker = { -- Enhances `select()`
+                    actions = {
+                        opencode_send = function(...)
+                            return require("opencode").snacks_picker_send(...)
+                        end,
+                    },
+                    win = {
+                        input = {
+                            keys = {
+                                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+                            },
+                        },
+                    },
+                },
+                terminal = {}, -- Enables the `snacks` provider
+            },
+        },
     },
     config = function()
+        ---@type opencode.Opts
         vim.g.opencode_opts = {
-            -- Your configuration, if any — see `lua/opencode/config.lua`
+            -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
         }
 
-        -- Required for `opts.auto_reload`
-        vim.opt.autoread = true
+        -- Required for `opts.events.reload`.
+        vim.o.autoread = true
 
+        -- Recommended/example keymaps.
         vim.keymap.set({ "n", "x" }, "<leader>oa", function()
             require("opencode").ask("@this: ", { submit = true })
-        end, { desc = "Ask about this" })
-        vim.keymap.set({ "n", "x" }, "<leader>os", function()
-            require("opencode").select()
-        end, { desc = "Select prompt" })
-        vim.keymap.set({ "n", "x" }, "<leader>o+", function()
-            require("opencode").prompt("@this")
-        end, { desc = "Add this" })
-        vim.keymap.set("n", "<leader>ot", function()
-            require("opencode").toggle()
-        end, { desc = "Toggle embedded" })
-        vim.keymap.set("n", "<leader>oc", function()
-            require("opencode").command()
-        end, { desc = "Select command" })
-        vim.keymap.set("n", "<leader>on", function()
-            require("opencode").command("session_new")
-        end, { desc = "New session" })
-        vim.keymap.set("n", "<leader>oi", function()
-            require("opencode").command("session_interrupt")
-        end, { desc = "Interrupt session" })
-        vim.keymap.set("n", "<leader>oA", function()
-            require("opencode").command("agent_cycle")
-        end, { desc = "Cycle selected agent" })
-        vim.keymap.set("n", "<S-C-u>", function()
-            require("opencode").command("messages_half_page_up")
-        end, { desc = "Messages half page up" })
-        vim.keymap.set("n", "<S-C-d>", function()
-            require("opencode").command("messages_half_page_down")
-        end, { desc = "Messages half page down" })
+        end, { desc = "Ask opencode…" })
     end,
 }
