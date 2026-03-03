c872e52a9d27998efb7481af86b391a18c55ac42
w3barsi <darcybalaga@gmail.com>
2026-03-03 15:18:08 +0800

    added more config for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -11,5 +11,23 @@ return {
         keys = {
             { "<C-e>", "<cmd>Neotree toggle<cr>" },
         },
+        opts = {
+            close_if_last_window = true,
+            window = {
+                position = "right",
+            },
+            filesystem = {
+                follow_current_file = {
+                    enabled = true, -- Auto-follow current file
+                    leave_dirs_open = false, -- Auto-collapse expanded dirs when moving to other files
+                },
+            },
+            buffers = {
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = false,
+                },
+            },
+        },
     },
 }
